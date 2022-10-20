bbdc0879057b347a20cae97ee1cf8d54bdda69c3
Mehdi Sakji <sakji.mehdi@gmail.com>
2022-10-20 18:18:19 +0200

    Update leadership.py

--- axoxlabs_web_scraper/axoxlabs_web_scraper/spiders/leadership.py
+++ axoxlabs_web_scraper/axoxlabs_web_scraper/spiders/leadership.py
@@ -1,9 +1,8 @@
 import scrapy
 
+class Leadership(scrapy.Spider):
 
-class SunNewsSpider(scrapy.Spider):
-
-    name = "sun_news"
+    name = "leadership"
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
 
     def start_requests(self):
@@ -12,10 +11,12 @@ class SunNewsSpider(scrapy.Spider):
         """
 
         categories_mapping = {
-            "politics": "politics",
-            "business": "business", "sporting-sun": "sports", "entertainment": "entertainment"}
+            "nigeria-news/world-news": "news", "nigeria-news/education-news/asuu-news": "news",
+            "politics": "politics", "nigeria-news/business-news": "business", "sports-news": "sports",
+            "nigeria-news/entertainment-news": "entertainment", "nigeria-news/education-news": "education",
+            "nigeria-news/health-news": "health"}
         lists_urls = [
-            "https://www.sunnewsonline.com/category/{}/".format(item) for item in categories_mapping.keys()]
+            "https://leadership.ng/{}".format(item) for item in categories_mapping.keys()]
         for url, category in zip(lists_urls, categories_mapping.keys()):
             yield scrapy.Request(
                 url=url, headers=self.headers, meta={"category": categories_mapping[category]}, callback=self.scrape_items)
@@ -32,36 +33,32 @@ class SunNewsSpider(scrapy.Spider):
 
     def scrape_page_items(self, response):
 
-        article_blocks = response.css(".jeg_inner_content")[0].css("article")
+        article_blocks = response.css(".jeg_post")
         for article in article_blocks:
             article_url = article.css("a::attr(href)").extract_first()
             yield scrapy.Request(
                 url=article_url, meta={"category": response.meta["category"]}, callback=self.scrape_item)
 
     def scrape_item(self, response):
+        headline = response.css(".jeg_post_title::text").extract_first()
         try:
-            headline = response.css(".jeg_post_title::text").extract_first()
-            try:
-                image_url = response.css(".featured_image")[0].css("a::attr(href)").extract_first()
-            except:
-                # No image
-                image_url = ""
-                pass
-            author = response.css(".jeg_author_name")[0].css("a::text").extract_first().strip()
-            posted_date = response.css(".jeg_meta_date")[0].css("a::text").extract_first().strip()
-            paragraphs_obj = response.css(".content-inner")[0].css("p")
-            paragraphs = [item.css("span::text").extract_first() for item in paragraphs_obj]
-            description = " ".join(paragraphs)
-            yield {
-                'headline': headline,
-                'image_url': image_url,
-                'author': author,
-                'posted_date': posted_date,
-                'description': description,
-                'newspaper_name': "The Sun News Paper",
-                'category': response.meta["category"],
-                'url': response.url
-            }
+            image_url = response.css(".featured_image")[0].css("a::attr(href)").extract_first()
         except:
-            print("url {} has empty content or different structure".format(response.url))
+            # No image
+            image_url = ""
             pass
+        authors_blocks = response.css("jeg_meta_author")[0].css("a::text").extract()
+        author = ", ".join([item.strip() for item in authors_blocks])
+        posted_date = response.css(".jeg_meta_date")[0].css("a::text").extract_first().strip()
+        paragraphs = response.css(".content-inner")[0].css("p::text").extract()
+        description = " ".join(paragraphs)
+        yield {
+            'headline': headline,
+            'image_url': image_url,
+            'author': author,
+            'posted_date': posted_date,
+            'description': description,
+            'newspaper_name': "The Sun News Paper",
+            'category': response.meta["category"],
+            'url': response.url
+        }
