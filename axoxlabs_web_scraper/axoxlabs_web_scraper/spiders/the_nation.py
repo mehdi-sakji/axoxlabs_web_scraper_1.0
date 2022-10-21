0fd33a189bc8ac42a921d7a3ec704fec137161b2
Mehdi Sakji <sakji.mehdi@gmail.com>
2022-10-21 16:23:06 +0200

    Update the_nation.py

--- axoxlabs_web_scraper/axoxlabs_web_scraper/spiders/the_nation.py
+++ axoxlabs_web_scraper/axoxlabs_web_scraper/spiders/the_nation.py
@@ -3,7 +3,9 @@ import scrapy
 class TheNationSpider(scrapy.Spider):
     
     name = "the_nation"
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
+    USER_AGENT='Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'
+    headers = {'User-Agent': USER_AGENT}
+    # headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
 
     def start_requests(self):
         """
