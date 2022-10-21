aaf961bc0634c7146e5868976f3b8921f2caf33e
Mehdi Sakji <sakji.mehdi@gmail.com>
2022-10-21 13:57:02 +0200

    Update tribune.py

--- axoxlabs_web_scraper/axoxlabs_web_scraper/spiders/tribune.py
+++ axoxlabs_web_scraper/axoxlabs_web_scraper/spiders/tribune.py
@@ -19,7 +19,7 @@ class TribuneSpider(scrapy.Spider):
             yield scrapy.Request(
                 url=url, headers=self.headers, meta={"category": category}, callback=self.scrape_items)
 
-    def scrape_items(self, response):
+   def scrape_items(self, response):
 
         article_blocks = response.css(".pt-cv-ifield")
         for article in article_blocks:
