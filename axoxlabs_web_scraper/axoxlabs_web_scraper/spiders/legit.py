8f9d9d6afee4e48a5fb41377c7d54ccea0f298a2
Mehdi Sakji <sakji.mehdi@gmail.com>
2022-10-21 18:18:03 +0200

    Update legit.py

--- axoxlabs_web_scraper/axoxlabs_web_scraper/spiders/legit.py
+++ axoxlabs_web_scraper/axoxlabs_web_scraper/spiders/legit.py
@@ -1,20 +1,61 @@
-"""
-https://www.legit.ng/ scraper.
-"""
-
 import scrapy
 
+class LegitSpider(scrapy.Spider):
 
-class LegitScraper(scrapy.Spider):
-    """
-    Spider for scraping https://www.legit.ng/ articles.
-    """
-
-    name = "legit_scraper"
+    name = "legit"
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
 
     def start_requests(self):
         """
         Yields list of categories URLs to scrape.
         """
 
-        # TODO add Selenium browser
+        categories_mapping = {
+            "politics": "politics", "business-economy": "business", "entertainment": "entertainment",
+            "nigeria": "news", "world": "news"}
+        # Sports need a custom spider
+        lists_urls = [
+            "https://www.legit.ng/{}".format(item) for item in categories_mapping.keys()]
+        for url, category in zip(lists_urls, categories_mapping.keys()):
+            yield scrapy.Request(
+                url=url, headers=self.headers, meta={"category": categories_mapping[category]}, callback=self.scrape_items)
+
+    # TODO integrate pagination OR selenium OR API to load more articles
+    def scrape_items(self, response):
+
+        header_articles_block = response.css(".l-taxonomy-page-hero__list")[0]
+        header_articles_urls = [
+            item.css("a::attr(href)").extract_first() for item in header_articles_block.css("article")]
+        body_articles_block = response.css(".js-articles")[0]
+        body_articles_urls = [
+            item.css("a::attr(href)").extract_first() for item in body_articles_block.css("article")]
+        
+        for url in header_articles_block + body_articles_block:
+            yield scrapy.Request(
+                url=url, meta={"category": response.meta["category"]}, callback=self.scrape_item)
+
+    def scrape_item(self, response):
+        
+        article_block = response.css(".js-article-body")[0]
+        headline = article_block.css(".c-main-headline::text").extract_first()
+        try:
+            image_url = article_block.css(".article-image")[0].css("img::attr(src)").extract_first()
+        except:
+            # No image
+            image_url = ""
+            pass
+        author = article_block.css(".post__info")[0].css(".c-article-info__author::text").extract_first().strip()
+        posted_date = article_block.css(".post__info")[0].css(".c-article-info__time::text").extract_first().strip()
+        
+        paragraphs = article_block.css(".post__content")[0].css("p::text").extract()
+        description = " ".join(paragraphs)
+        yield {
+            'headline': headline,
+            'image_url': image_url,
+            'author': author,
+            'posted_date': posted_date,
+            'description': description,
+            'newspaper_name': "Legit NG",
+            'category': response.meta["category"],
+            'url': response.url
+        }
